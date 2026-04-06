156d898dc68e4c4acf2b289d37f2d6a825dc69b9
Carol <162912256+carolmarques09@users.noreply.github.com>
2026-04-06 16:55:17 -0300

    Delete spec/import_sales_spec.rb

--- spec/import_sales_spec.rb
+++
@@ -1,9 +0,0 @@
-require './app/services/import_sales'
-
-describe ImportSales do
-  it 'calculates total without crashing' do
-    file = File.open('example_input.tab')
-    total = ImportSales.call(file)
-    expect(total).to be > 0
-  end
-end
